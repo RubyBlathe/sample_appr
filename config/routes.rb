a5a389b9a88bec529daf561a1ba9f30825f021c2
Christopher Clubb <rupert_murdoc@yahoo.com>
2012-06-01 01:29:17 -0700

    Make a basic User model (including secure passwords)

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,7 @@ SampleAppr::Application.routes.draw do
 
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
-  match '/contact', to: 'static_pages#contact'
-  match '/contact', to: 'static_pages#contact'  
+  match '/contact', to: 'static_pages#contact' 
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
