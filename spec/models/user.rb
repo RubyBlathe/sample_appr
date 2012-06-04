6c95db90c1f96bb95ab32ce9888c0060d43cec5b
Christopher Clubb <rupert_murdoc@yahoo.com>
2012-06-03 23:20:29 -0700

    Add user following

---
+++ spec/models/user.rb
@@ -0,0 +1,7 @@
+class User < ActiveRecord::Base
+
+  has_many :microposts, dependent: :destroy
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+ 
+end
